64c9df86ff3149299bf57ab2e8256278fb3833ea
lovischen <lovischen@tencent.com>
2026-08-17 16:19:32 +0800

    腐烂橘子优化

--- leetcode/bfs/腐烂的橘子.py
+++ leetcode/bfs/腐烂的橘子.py
@@ -1,12 +1,9 @@
 """
-腐烂的橘子
 在给定的 m x n 网格 grid 中，每个单元格可以有以下三个值之一：
-
 值 0 代表空单元格；
 值 1 代表新鲜橘子；
 值 2 代表腐烂的橘子。
 每分钟，腐烂的橘子 周围 4 个方向上相邻 的新鲜橘子都会腐烂。
-
 返回 直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1 。
 
 输入：grid = [[2,1,1],[1,1,0],[0,1,1]]
@@ -14,6 +11,25 @@
 """
 from typing import List
 from collections import deque
+class Solution:
+    def rangeRotting(self,grid: List[List[int]]) -> int:
+        """解法1：bfs"""
+        row = len(grid)
+        col = len(grid[0])
+        time = 0
+
+        # 计算腐烂橘子和新鲜橘子 列表位置
+        rotten_origan = {(i,j) for i in range(row) for j in range(col) if grid[i][j] == 2}
+        fresh_origan = {(i,j) for i in range(row) for j in range(col) if grid[i][j] == 1}
+
+        while fresh_origan:
+            if not fresh_origan: return -1
+            rotten_origan += {(i+di,j+dj) for i,j in rotten_origan for di,dj in [(0,1),(0,-1),(1,0),(-1,0)] if (i+di,j+dj) in fresh_origan}
+            fresh_origan -= rotten_origan
+            time += 1
+
+        return time
+
 
 class solution():
     def orangeRotting(self,grid: List[List[int]]) -> int:
