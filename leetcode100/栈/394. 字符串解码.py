fedcb7108ce63faf2662c74f681b550ae726e21d
lovischen <lovischen@tencent.com>
2026-08-18 16:08:38 +0800

    leetcode100:【单调栈】

--- leetcode100/栈/394. 字符串解码.py
+++ leetcode100/栈/394. 字符串解码.py
@@ -3,13 +3,12 @@ def decodeString(s: str) -> str:
     给定一个经过编码的字符串，返回它解码后的字符串。
     编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
     你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
-    此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+    此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
     输入：s = "3[a2[bc]]"
-    输出：""
+    输出："abcbcabcbcabcbc
     """
     # 思路 栈＋递归
     stack = []
-    cnt = ""
     for str in s:
         if str != ']':
             stack.append(str)
@@ -27,8 +26,11 @@ def decodeString(s: str) -> str:
     return cnt
 
 if __name__ == "__main__":
-    s = "100[leetcode100]"
+    # s = "100[leetcode100]"
+    s = "3[a]2[bc]"
+    # s = "3[a2[bc]"
     print(decodeString(s))
+
 
 
 
