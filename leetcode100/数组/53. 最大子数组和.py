8ce49f52a258f9930f9084b3b66f866ddd3e67ef
ScholarChen20 <1523910137@qq.com>
2026-09-15 10:19:24 +0800

    八股补充优化【面试】

--- leetcode100/数组/53. 最大子数组和.py
+++ leetcode100/数组/53. 最大子数组和.py
@@ -1,10 +1,7 @@
 """
 53. 最大子数组和
 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
-
 子数组是数组中的一个连续部分。
-
-
 
 示例 1：
 
@@ -26,6 +23,19 @@ class Solution:
         for i in range(1, n):
             dp[i] = max(dp[i - 1] + nums[i], nums[i])
         return max(dp)
+
+    def maxSubArray2(self, nums: list[int]) -> int:
+        """
+        贪心算法, 当前元素和前一个元素的和比较大，则当前元素和前一个元素的和，否则当前元素和前一个元素的和为0
+        :param nums:
+        :return:
+        """
+        max_sum = nums[0]
+        current_sum = nums[0]
+        for i in range(1, len(nums)):
+            current_sum = max(current_sum + nums[i], nums[i])
+            max_sum = max(max_sum, current_sum)
+        return max_sum
 
 if __name__ == '__main__':
     s = Solution()
